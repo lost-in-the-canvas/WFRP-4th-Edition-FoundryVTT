7d524363c4c8cdb9fc7a981764326dc0e574311c
Russell <russellthurman94@gmail.com>
2019-03-30 01:55:27 -0500

    Initial attempt at conversion
    Doesn't work yet

--- wfrp4e/wfrp4e.js
+++ wfrp4e/wfrp4e.js
@@ -189,25 +189,7 @@ class Actor5e extends Actor {
     if ( actorData.type === "character" ) this._prepareCharacterData(data);
     else if ( actorData.type === "npc" ) this._prepareNPCData(data);
 
-    // Ability modifiers and saves
-    for (let abl of Object.values(data.abilities)) {
-      abl.mod = Math.floor((abl.value - 10) / 2);
-      abl.save = abl.mod + ((abl.proficient || 0) * data.attributes.prof.value);
-    }
-
-    // Skill modifiers
-    for (let skl of Object.values(data.skills)) {
-      skl.value = parseFloat(skl.value || 0);
-      skl.mod = data.abilities[skl.ability].mod + Math.floor(skl.value * data.attributes.prof.value);
-    }
-
-    // Attributes
-    data.attributes.init.mod = data.abilities.dex.mod + (data.attributes.init.value || 0);
-    data.attributes.ac.min = 10 + data.abilities.dex.mod;
-
-    // Spell DC
-    let spellAbl = data.attributes.spellcasting.value || "int";
-    data.attributes.spelldc.value = 8 + data.attributes.prof.value + data.abilities[spellAbl].mod;
+    data.status.wounds.max = 2 * Math.floor(data.characteristics.t.value/10) + Math.floor(data.characteristics.s.value/10) + Math.floor(data.characteristics.wp.value/10)
 
     // Return the prepared Actor data
     return actorData;
@@ -220,13 +202,6 @@ class Actor5e extends Actor {
    */
   _prepareCharacterData(data) {
 
-    // Level, experience, and proficiency
-    data.details.level.value = parseInt(data.details.level.value);
-    data.details.xp.max = this.getLevelExp(data.details.level.value || 1);
-    let prior = this.getLevelExp(data.details.level.value - 1 || 0),
-          req = data.details.xp.max - prior;
-    data.details.xp.pct = Math.min(Math.round((data.details.xp.value -prior) * 100 / req), 99.5);
-    data.attributes.prof.value = Math.floor((data.details.level.value + 7) / 4);
   }
 
   /* -------------------------------------------- */
@@ -236,38 +211,6 @@ class Actor5e extends Actor {
    */
   _prepareNPCData(data) {
 
-    // CR, kill exp, and proficiency
-    data.details.cr.value = parseFloat(data.details.cr.value) || 0;
-    data.details.xp.value = this.getCRExp(data.details.cr.value);
-    data.attributes.prof.value = Math.floor((Math.max(data.details.cr.value, 1) + 7) / 4);
-  }
-
-  /* -------------------------------------------- */
-
-  /**
-   * Return the amount of experience required to gain a certain character level.
-   * @param level {Number}  The desired level
-   * @return {Number}       The XP required
-   */
-  getLevelExp(level) {
-    const levels = [0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000,
-      120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000];
-    return levels[Math.min(level, levels.length - 1)];
-  }
-
-  /* -------------------------------------------- */
-
-  /**
-   * Return the amount of experience granted by killing a creature of a certain CR.
-   * @param cr {Number}     The creature's challenge rating
-   * @return {Number}       The amount of experience granted per kill
-   */
-  getCRExp(cr) {
-    if (cr < 1.0) return Math.max(200 * cr, 10);
-    let _ = undefined;
-    const xps = [10, 200, 450, 700, 1100, 1800, 2300, 2900, 3900, 5000, 5900, 18000, 20000, 22000,
-        25000, 27500, 30000, 32500, 36500, _, _, _, _, _, 155000];
-    return xps[cr];
   }
 
   /* -------------------------------------------- */
@@ -279,7 +222,7 @@ class Actor5e extends Actor {
    * Prompt the user for input regarding Advantage/Disadvantage and any Situational Bonus
    * @param skill {String}    The skill id
    */
-  rollSkill(skillName) {
+  rollSkill(event, skillName) {
     let skl = this.data.data.skills[skillName],
       parts = ["@mod"],
       flavor = `${skl.label} Skill Check`;
@@ -303,28 +246,41 @@ class Actor5e extends Actor {
    */
   rollAbility(abilityId) {
     let abl = this.data.data.abilities[abilityId];
-    this.rollAbilityTest(abilityId)
+    new Dialog({
+      title: `${abl.label} Ability Check`,
+      content: `<p>What type of ${abl.label} check?</p>`,
+      buttons: {
+        test: {
+          label: "Ability Test",
+          callback: () => this.rollAbilityTest(abilityId)
+        },
+        save: {
+          label: "Saving Throw",
+          callback: () => this.rollAbilitySave(abilityId)
+        }
+      }
+    }).render(true);
   }
 
   /* -------------------------------------------- */
 
   /**
    * Roll an Ability Test
-   * Prompt the user for input regardi g Advantage/Disadvantage and any Situational Bonus
+   * Prompt the user for input regarding Advantage/Disadvantage and any Situational Bonus
    * @param abilityId {String}    The ability ID (e.g. "str")
    */
   rollAbilityTest(abilityId) {
     let abl = this.data.data.abilities[abilityId],
-        parts = {"target":Number(abl.value)},
+        parts = ["@mod"],
         flavor = `${abl.label} Ability Test`;
 
     // Call the roll helper utility
-    Dice5e.d100Roll({
+    Dice5e.d20Roll({
       event: event,
       parts: parts,
       data: {mod: abl.mod},
       title: flavor,
-      alias: this.data.name,
+      alias: this.actor,
     });
   }
 
@@ -375,7 +331,42 @@ class Actor5e extends Actor {
       }
     }).render(true);
   }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Apply rolled dice damage to the token or tokens which are currently controlled.
+   * This allows for damage to be scaled by a multiplier to account for healing, critical hits, or resistance
+   *
+   * @param {HTMLElement} roll    The chat entry which contains the roll data
+   * @param {Number} multiplier   A damage multiplier to apply to the rolled damage.
+   */
+  static applyDamage(roll, multiplier) {
+    let value = Math.floor(parseFloat(roll.find('.dice-total').text()) * multiplier);
+
+    // Filter tokens to which damage can be applied
+    canvas.tokens.controlledTokens.filter(t => {
+      if ( t.actor && t.data.actorLink ) return true;
+      else if ( t.data.bar1.attribute === "attributes.hp" || t.data.bar2.attribute === "attributes.hp" ) return true;
+      return false;
+    }).forEach(t => {
+
+      // Linked Tokens - update Actor
+      if ( t.actor && t.data.actorLink ) {
+        let hp = parseInt(t.actor.data.data.attributes.hp.value),
+            max = parseInt(t.actor.data.data.attributes.hp.max);
+        t.actor.update({"data.attributes.hp.value": Math.clamped(hp - value, 0, max)});
+      }
+
+      // Unlinked Tokens - update Token directly
+      else {
+        let bar = (t.data.bar1.attribute === "attributes.hp") ? "bar1" : "bar2";
+        t.update(canvas.id, {[`${bar}.value`]: Math.clamped(t.data[bar].value - value, 0, t.data[bar].max)});
+      }
+    });
+  }
 }
+
 
 // Assign the actor class to the CONFIG
 CONFIG.Actor.entityClass = Actor5e;
@@ -383,6 +374,7 @@ CONFIG.Actor.entityClass = Actor5e;
 /**
  * Extend the basic ActorSheet class to do all the D&D5e things!
  */
+
 class Actor5eSheet extends ActorSheet {
 
   /**
@@ -422,36 +414,6 @@ class Actor5eSheet extends ActorSheet {
   getData() {
     const sheetData = super.getData();
 
-    // Level and CR
-    if ( sheetData.actor.type === "npc" ) {
-      let cr = sheetData.data.details.cr;
-      let crs = {0: "0", 0.125: "1/8", 0.25: "1/4", 0.5: "1/2"};
-      cr.str = (cr.value >= 1) ? String(cr.value) : crs[cr.value] || 0;
-    }
-
-    // Ability proficiency
-    for ( let abl of Object.values(sheetData.data.abilities)) {
-      abl.icon = this._getProficiencyIcon(abl.proficient);
-      abl.hover = CONFIG.proficiencyLevels[abl.proficient];
-    }
-
-    // Update skill labels
-    for ( let skl of Object.values(sheetData.data.skills)) {
-      skl.ability = sheetData.data.abilities[skl.ability].label.substring(0, 3);
-      skl.icon = this._getProficiencyIcon(skl.value);
-      skl.hover = CONFIG.proficiencyLevels[skl.value];
-    }
-
-    // Clear some values
-    let res = sheetData.data.resources;
-    if ( res.primary && res.primary.value === 0 ) delete res.primary.value;
-    if ( res.primary && res.primary.max === 0 ) delete res.primary.max;
-    if ( res.secondary && res.secondary.value === 0 ) delete res.secondary.value;
-    if ( res.secondary && res.secondary.max === 0 ) delete res.secondary.max;
-    let hp = sheetData.data.attributes.hp;
-    if ( hp.temp === 0 ) delete hp.temp;
-    if ( hp.tempmax === 0 ) delete hp.tempmax;
-
     // Prepare owned items
     if ( this.actorType === "character" ) this._prepareCharacterItems(sheetData.actor);
     else if ( this.actorType === "npc" ) this._prepareNPCItems(sheetData.actor);
@@ -469,7 +431,7 @@ class Actor5eSheet extends ActorSheet {
   _prepareCharacterItems(actorData) {
 
     // Inventory
-    const inventory = {
+  /*  const inventory = {
       weapon: { label: "Weapons", items: [] },
       equipment: { label: "Equipment", items: [] },
       consumable: { label: "Consumables", items: [] },
@@ -537,82 +499,7 @@ class Actor5eSheet extends ActorSheet {
       value: Math.round(totalWeight * 10) / 10,
     };
     enc.pct = Math.min(enc.value * 100 / enc.max, 99);
-    actorData.data.attributes.encumbrance = enc;
-  }
-
-  /* -------------------------------------------- */
-
-  /**
-   * Organize and classify Items for NPC sheets
-   * @private
-   */
-  _prepareNPCItems(actorData) {
-
-    // Actions
-    const features = {
-      weapons: {label: "Weapons", items: [], type: "weapon" },
-      actions: { label: "Actions", items: [], type: "feat" },
-      passive: { label: "Features", items: [], type: "feat" },
-      equipment: { label: "Equipment", items: [], type: "armor" }
-    };
-
-    // Spellbook
-    const spellbook = {};
-
-    // Iterate through items, allocating to containers
-    for ( let i of actorData.items ) {
-      i.img = i.img || DEFAULT_TOKEN;
-
-      // Spells
-      if ( i.type === "spell" ) {
-        let lvl = i.data.level.value || 0;
-        spellbook[lvl] = spellbook[lvl] || {
-          isCantrip: lvl === 0,
-          label: CONFIG.spellLevels[lvl],
-          spells: [],
-          uses: actorData.data.spells["spell"+lvl].value || 0,
-          slots: actorData.data.spells["spell"+lvl].max || 0
-        };
-        i.data.school.str = CONFIG.spellSchools[i.data.school.value];
-        spellbook[lvl].spells.push(i);
-      }
-
-      // Features
-      else if ( i.type === "weapon" ) features.weapons.items.push(i);
-      else if ( i.type === "feat" ) {
-        if ( i.data.featType.value === "passive" ) features.passive.items.push(i);
-        else features.actions.items.push(i);
-      }
-      else if (["equipment", "consumable", "tool", "backpack"].includes(i.type)) features.equipment.items.push(i);
-    }
-
-    // Assign and return
-    actorData.features = features;
-    actorData.spellbook = spellbook;
-  }
-
-  /* -------------------------------------------- */
-
-  _cycleSkillProficiency(level) {
-    const levels = [0, 1, 0.5, 2];
-    let idx = levels.indexOf(level);
-    return levels[(idx === levels.length - 1) ? 0 : idx + 1]
-  }
-
-  /* -------------------------------------------- */
-
-  /**
-   * Get the font-awesome icon used to display a certain level of skill proficiency
-   * @private
-   */
-  _getProficiencyIcon(level) {
-    const icons = {
-      0: '<i class="far fa-circle"></i>',
-      0.5: '<i class="fas fa-adjust"></i>',
-      1: '<i class="fas fa-check"></i>',
-      2: '<i class="fas fa-check-double"></i>'
-    };
-    return icons[level];
+    actorData.data.attributes.encumbrance = enc;*/
   }
 
   /* -------------------------------------------- */
@@ -649,42 +536,23 @@ class Actor5eSheet extends ActorSheet {
     /*  Abilities and Skills
      /* -------------------------------------------- */
 
-    // Ability Proficiency
-    html.find('.ability-proficiency').click(ev => {
-      let field = $(ev.currentTarget).siblings('input[type="hidden"]');
-      this.actor.update({[field[0].name]: 1 - parseInt(field[0].value)});
-    });
-
     // Ability Checks
     html.find('.ability-name').click(ev => {
       let abl = ev.currentTarget.parentElement.getAttribute("data-ability");
       this.actor.rollAbility(abl);
     });
 
-    // Toggle Skill Proficiency
-    html.find('.skill-proficiency').click(ev => {
-      let field = $(ev.currentTarget).siblings('input[type="hidden"]');
-      field.val(this._cycleSkillProficiency(parseFloat(field.val())));
-      let formData = validateForm(field.parents('form')[0]);
-      this.actor.update(formData, true);
-    });
-
     // Roll Skill Checks
     html.find('.skill-name').click(ev => {
       let skl = ev.currentTarget.parentElement.getAttribute("data-skill");
-      this.actor.rollSkill(skl);
+      this.actor.rollSkill(ev, skl);
     });
 
     /* -------------------------------------------- */
     /*  Rollable Items                              */
     /* -------------------------------------------- */
 
-    html.find('.item .rollable').click(ev => {
-      let itemId = Number($(ev.currentTarget).parents(".item").attr("data-item-id")),
-        Item = CONFIG.Item.entityClass,
-        item = new Item(this.actor.items.find(i => i.id === itemId), this.actor);
-      item.roll();
-    });
+    html.find('.item .rollable').click(event => this._onRollItemCard(event));
 
     /* -------------------------------------------- */
     /*  Inventory
@@ -716,26 +584,6 @@ class Actor5eSheet extends ActorSheet {
     /*  Miscellaneous
     /* -------------------------------------------- */
 
-    /* Short Rest */
-    html.find('.short-rest').click(ev => {
-      ev.preventDefault();
-      ui.notifications.info("Short Rest functionality not yet implemented, just testing the button!");
-    });
-
-    /* Short Rest */
-    html.find('.long-rest').click(ev => {
-      ev.preventDefault();
-      ui.notifications.info("Long Rest functionality not yet implemented, just testing the button!");
-    });
-
-    /* Roll NPC HP */
-    html.find('.npc-roll-hp').click(ev => {
-      let ad = this.actor.data.data;
-      let hp = new Roll(ad.attributes.hp.formula).roll().total;
-      AudioHelper.play({src: CONFIG.sounds.dice, volume: 0.8});
-      this.actor.update({"data.attributes.hp.value": hp, "data.attributes.hp.max": hp}, true);
-    });
-
     /* Item Dragging */
     let handler = ev => this._onDragItemStart(ev);
     html.find('.item').each((i, li) => {
@@ -756,20 +604,12 @@ class Actor5eSheet extends ActorSheet {
    */
   _updateObject(event, formData) {
 
-    // Format NPC Challenge Rating
-    if (this.actor.data.type === "npc") {
-      let cr = this.form["data.details.cr.value"],
-      val = cr.value,
-      crs = {"1/8": 0.125, "1/4": 0.25, "1/2": 0.5};
-      cr.value = crs[val] || val;
-    }
-
     // Parent ActorSheet update steps
     super._updateObject(event, formData);
   }
 
   /* -------------------------------------------- */
-  /*  Drag and Drop                               */
+  /*  Event Listeners and Handlers                */
   /* -------------------------------------------- */
 
   _onDragItemStart(event) {
@@ -780,24 +620,26 @@ class Actor5eSheet extends ActorSheet {
       id: itemId
     }));
   }
-}
 
+  /* -------------------------------------------- */
+
+  /**
+   * Handle rolling of an item from the Actor sheet, obtaining the Item instance and dispatching to it's roll method
+   * @private
+   */
+  _onRollItemCard(event) {
+    event.preventDefault();
+    let itemId = Number($(event.currentTarget).parents(".item").attr("data-item-id")),
+        Item = CONFIG.Item.entityClass,
+        item = new Item(this.actor.items.find(i => i.id === itemId), this.actor);
+    item.roll();
+  }
+}
 
 /* -------------------------------------------- */
 
 
 CONFIG.Actor.sheetClass = Actor5eSheet;
-
-
-/**
- * Skill Proficiency Levels
- */
-CONFIG.proficiencyLevels = {
-  0: "Not Proficient",
-  1: "Proficient",
-  0.5: "Jack of all Trades",
-  2: "Expertise"
-};
 
 
 /* -------------------------------------------- */
