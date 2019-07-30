0f3b898852d279d97795929b9afff2e8ace0daaa
Russell <russellthurman94@gmail.com>
2019-07-29 19:28:55 -0500

    Fixed rollModes, added Cast/Channel choice template
    Fixes #93
    Fixes #81

--- wfrp4e/wfrp4e.js
+++ wfrp4e/wfrp4e.js
@@ -751,7 +751,6 @@ class DiceWFRP {
     renderTemplate(dialogOptions.template, dialogOptions.data).then(dlg => {
       new Dialog({
           title: dialogOptions.title,
-          class: "red",
           content: dlg,
           buttons: dialogOptions.buttons,
           close: html => dialogOptions.callback(html, roll)
@@ -1129,9 +1128,12 @@ class DiceWFRP {
  static renderRollCard(chatOptions, testData) {
    let chatData = {
      title : chatOptions.title,
-     testData : testData,
-     gm : game.user.isGM
+     testData : testData
    }
+
+   if ( ["gmroll", "blindroll"].includes(chatOptions.rollMode) ) chatOptions["whisper"] = ChatMessage.getWhisperIDs("GM");
+   if ( chatOptions.rollMode === "blindroll" ) chatOptions["blind"] = true;
+
   // Generate HTML from the requested chat template
   return renderTemplate(chatOptions.template, chatData).then(html => {
 
@@ -1173,26 +1175,24 @@ class DiceWFRP {
       else if (ev.button == 2)
       {
         renderTemplate('public/systems/wfrp4e/templates/chat/table-dialog.html').then(html => {
+          new Dialog({
+            title: "Table Modifier",
+            content: html,
+            buttons: {
+              roll: {
+                label: "Roll",
+                callback: (html) => {
+                  let tableModifier = html.find('[name="tableModifier"]').val();
+                  let minOne = html.find('[name="minOne"]').is(':checked');
+                  html = WFRP_Tables.formatChatRoll($(ev.currentTarget).attr("data-table"), {modifier: tableModifier, minOne : minOne});
+                  ChatMessage.create({content : html, user : senderId})
 
-        new Dialog({
-          title: "Table Modifier",
-          content: html,
-          buttons: {
-            roll: {
-              label: "Roll",
-              callback: (html) => {
-                let tableModifier = html.find('[name="tableModifier"]').val();
-                let minOne = html.find('[name="minOne"]').is(':checked');
-                html = WFRP_Tables.formatChatRoll($(ev.currentTarget).attr("data-table"), {modifier: tableModifier, minOne : minOne});
-                ChatMessage.create({content : html, user : senderId})
-
-              }
+                }
+              },
             },
-          },
-          default: 'roll'
-        }).render(true);
-      })
-
+            default: 'roll'
+          }).render(true);
+        })
       }
 
 
@@ -2232,26 +2232,27 @@ class ActorWfrp4e extends Actor {
 
   // Roll spell Dialog - choose between Casting or Channelling
   rollSpell(spell, options) {
-    new Dialog({
-      title: "Cast or Channell",
-      content: '<p>Cast or Channell this spell?</p>',
-      buttons: {
-        cast: {
-          label: "Cast",
-          callback: dlg => {
-            this.rollCast(spell, options);
-          }
+    renderTemplate("public/systems/wfrp4e/templates/chat/cast-channel-dialog.html").then(dlg => {
+      new Dialog({
+        title: "Cast or Channell",
+        content: dlg,
+        buttons: {
+          cast: {
+            label: "Cast",
+            callback: btn => {
+              this.rollCast(spell, options);
+            }
+          },
+          channell: {
+            label: "Channell",
+            callback: btn => {
+              this.rollChannell(spell, options);
+            }
+          },
         },
-        channell: {
-          label: "Channell",
-          callback: dlg => {
-            this.rollChannell(spell, options);
-
-          }
-        },
-      },
-      default: 'cast'
-    }).render(true);
+        default: 'cast'
+      }).render(true);
+    })
   }
 
   /**
