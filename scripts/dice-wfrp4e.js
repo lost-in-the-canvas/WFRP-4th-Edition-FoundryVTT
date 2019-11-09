800b5cf8e8420569adc978cece4cb899be4e4edf
Russell <russellthurman94@gmail.com>
2019-11-08 23:50:13 -0600

    Manual chat roll option
    closes #223

--- scripts/dice-wfrp4e.js
+++ scripts/dice-wfrp4e.js
@@ -569,6 +569,12 @@ class DiceWFRP {
   * @param {Object} testData - Test results, values to display, etc.
   */
    static renderRollCard(chatOptions, testData, rerenderMessage) {
+
+    if (game.settings.get("wfrp4e", "manualChatCards") && !rerenderMessage)
+    {
+      testData.roll = testData.SL = null;
+    }
+    
      let chatData = {
        title : chatOptions.title,
        testData : testData,
@@ -590,11 +596,24 @@ class DiceWFRP {
 
     if (!rerenderMessage)
     {
-
       // Generate HTML from the requested chat template
       return renderTemplate(chatOptions.template, chatData).then(html => {
-
         // Emit the HTML as a chat message
+        if (game.settings.get("wfrp4e", "manualChatCards"))
+        {
+          let blank = $(html)
+          let elementsToToggle = blank.find(".display-toggle")
+    
+          for (let elem of elementsToToggle)
+          {
+            if (elem.style.display == "none")
+              elem.style.display = ""
+            else 
+              elem.style.display = "none"
+          }
+          html = blank.html();
+        }
+        
         chatOptions["content"] = html;
 
         ChatMessage.create(chatOptions, false);
@@ -768,15 +787,7 @@ class DiceWFRP {
       // Chat card actions
       html.on('click', '.edit-toggle', ev => {
         ev.preventDefault();
-        let elementsToToggle = $(ev.currentTarget).parents(".chat-card").find(".display-toggle")
-        // Extract card data
-        for (let elem of elementsToToggle)
-        {
-          if (elem.style.display == "none")
-            elem.style.display = ""
-          else 
-            elem.style.display = "none"
-        }
+        this.toggleEditable(ev.currentTarget)
       });
 
       html.on("click", '.item-property', event => {
@@ -804,5 +815,20 @@ class DiceWFRP {
           opposeResult.result = defender.name + " won by " + differenceSL + " SL";
         }
         return opposeResult;
+    }
+
+    static toggleEditable(html)
+    {
+      let elementsToToggle = $(html).parents(".chat-card").find(".display-toggle")
+      if (!elementsToToggle.length)
+        elementsToToggle = $(html).find(".display-toggle")
+
+      for (let elem of elementsToToggle)
+      {
+        if (elem.style.display == "none")
+          elem.style.display = ""
+        else 
+          elem.style.display = "none"
+      }
     }
   }
