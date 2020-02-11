75c54195a787d84b5871d7f9d33390a295201947
Russell <russellthurman94@gmail.com>
2020-02-10 23:48:28 -0600

    Browser progress - checkboxes

--- scripts/wfrp-browser.js
+++ scripts/wfrp-browser.js
@@ -1,18 +1,34 @@
 class BrowserWfrp4e extends Application
 {
-
-  constructor(app)  
+  constructor(app)
   {
-    super(app);
-        
-    // load settings
-    Hooks.on('ready', e => {
+    super(app)
 
-      game.packs[5].getContent().then(items => {
-        this.items = items;
-      });
+    this.filters = {
+      type : {
+        "ammunition" : false,
+        "armour" : false,
+        "career" : false,
+        "container" : false,
+        "critical" : false,
+        "disease" : false,
+        "injury" : false,
+        "money" : false,
+        "mutation" : false,
+        "prayer" : false,
+        "psychology" : false,
+        "talent" : false,
+        "trapping" : false,
+        "skill" : false,
+        "spell" : false,
+        "trait" : false,
+        "weapon" : false
+      },
+      attribute : {
+        name : ""
+      }
+    }
     
-    });
   }
 
   static get defaultOptions() 
@@ -32,12 +48,77 @@ class BrowserWfrp4e extends Application
   getData() {
     let data = super.getData();
 
-    data.itemTypes = game.system.template.Item.types;
+    data.filters = this.filters;
 
-    data.items = this.items;
+    data.items = this.applyFilter(this.items);
 
     return data;
   }
+
+  async loadItems()
+  {
+    this.items = [];
+    for (let p of game.packs)
+    {
+      if (p.metadata.entity == "Item")
+      {
+        await p.getContent().then(content => {
+          this.items = this.items.concat(content)
+        })
+      }
+    }
+  }
+
+
+  applyFilter(items)
+  {
+    let noItemFilter = true;
+    let filteredItems = [];
+    for (let filter in this.filters.type)
+    {
+      if (this.filters.type[filter])
+      {
+        filteredItems = filteredItems.concat(items.filter(i => i.data.type == filter))
+        noItemFilter = false;
+      }
+    }
+
+    
+    if (noItemFilter)
+      filteredItems = items;
+
+    for (let filter in this.filters.attribute)
+    {
+      if (this.filters.attribute[filter])
+      {
+        switch(filter)
+        {
+          case "name" :
+            filteredItems = filteredItems.filter(i => i.data.name.toLowerCase().includes(this.filters.attribute.name.toLowerCase()))
+            console.log("NAME FILTER")
+            noItemFilter = false;
+
+        }
+      }
+    }
+    
+    return filteredItems.sort((a, b) => (a.data.name > b.data.name) ? 1 : -1);
+  }
+
+
+  activateListeners(html)
+  {
+    html.on("click", ".filter", ev => {
+      this.filters.type[$(ev.currentTarget).attr("data-filter")] = $(ev.currentTarget).is(":checked");
+      this.render(true);
+    })
+
+    html.on("keyup", ".name-filter", ev => {
+      this.filters.attribute.name = $(ev.currentTarget).val();
+      this.render(true);
+    })
+  }
+
 }
 
 Hooks.on("renderCompendiumDirectory", (app, html, data) => {
@@ -55,4 +136,9 @@ Hooks.on("renderCompendiumDirectory", (app, html, data) => {
 Hooks.on('init', () => {
   if (!game.wfrpbrowser)
     game.wfrpbrowser = new BrowserWfrp4e();
+})
+
+Hooks.on('ready', () => {
+  if (game.user.isGM)
+    game.wfrpbrowser.loadItems();
 })
