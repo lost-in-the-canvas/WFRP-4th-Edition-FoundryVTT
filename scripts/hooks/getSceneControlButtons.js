940c2ef323b3decb7022203507f288eb4acd3d06
Russell <russellthurman94@gmail.com>
2020-03-08 01:27:12 -0600

    Morrslieb Darkness

--- scripts/hooks/getSceneControlButtons.js
+++ scripts/hooks/getSceneControlButtons.js
@@ -7,7 +7,12 @@ Hooks.on("getSceneControlButtons", (buttons) => {
       button: true,
       icon : "fas fa-circle",
       name: "morrslieb",
-      title: "???",
-      onClick : () => {console.log("hello")}
+      title: canvas.scene.data.flags.core.darknessColor != CONFIG.Canvas.darknessColor? "Morrslieb Darkness Color - Currently On" : "Morrslieb Darkness Color - Currently Off",
+      onClick : async () => {
+        if (canvas.scene.getFlag("core", "darknessColor") == CONFIG.Canvas.darknessColor)
+          await canvas.scene.setFlag("core", "darknessColor", colorStringToHex("#006633"))
+        else
+          await canvas.scene.setFlag("core", "darknessColor", CONFIG.Canvas.darknessColor)        
+      }
     })
   })
