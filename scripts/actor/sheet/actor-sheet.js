ee746901cf09b060d7927949544831f7b8dac0da
Russell <russellthurman94@gmail.com>
2020-04-25 15:34:18 -0500

    Fix damaging shields localization

--- scripts/actor/sheet/actor-sheet.js
+++ scripts/actor/sheet/actor-sheet.js
@@ -560,13 +560,13 @@ class ActorSheetWfrp4e extends ActorSheet {
   // Damage a shield item by clicking on the shield AP amount in the combat tab
   html.find(".shield-total").mousedown(ev => {
     let weapons = this.actor.prepareItems().weapons
-    let shields = weapons.filter(w => w.properties.qualities.find(p => p.includes("Shield")))
+    let shields = weapons.filter(w => w.properties.qualities.find(p => p.toLowerCase().includes(game.i18n.localize("PROPERTY.Shield").toLowerCase())))
     let shieldDamaged = false;
     // If for some reason using multiple shields...damage the first one available 
     for (let s of shields)
     {
       let shield = duplicate(this.actor.getEmbeddedEntity("OwnedItem", s._id));
-      let shieldQualityValue = s.properties.qualities.find(p => p.includes("Shield")).split(" ")[1];
+      let shieldQualityValue = s.properties.qualities.find(p => p.toLowerCase().includes(game.i18n.localize("PROPERTY.Shield"))).split(" ")[1];
       
       if (!shield.data.APdamage)
         shield.data.APdamage = 0;
