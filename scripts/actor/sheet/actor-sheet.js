6efec69c810da2d30ea38718c739d21d2aa00a84
Russell <russellthurman94@gmail.com>
2020-04-29 00:13:01 -0500

    Fix property parsing and browser item editing
    Fix certain properties not being displayed (trap blade), fixed editing compendium items from the browser

--- scripts/actor/sheet/actor-sheet.js
+++ scripts/actor/sheet/actor-sheet.js
@@ -1352,7 +1352,7 @@ class ActorSheetWfrp4e extends ActorSheet {
     }
     else // Otherwise, just lookup the key for the property and use that to lookup the description
     {
-      propertyKey = WFRP_Utility.findKey(property.split(" ")[0], properties)
+      propertyKey = WFRP_Utility.findKey(WFRP_Utility.parsePropertyName(property), properties)
     }
   
     let propertyDescription = "<b>" + property + "</b>" + ": " + propertyDescr[propertyKey];
